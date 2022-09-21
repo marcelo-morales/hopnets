6aaf480a30bd86a76f33b98d2f8613c6cd729ec4
marcelomorales <lmoral10@jhu.edu>
2022-09-21 16:17:21 -0400

    added igraph and matplotliub network graphs

---
+++ random_graph/randomzied_graph.py
@@ -0,0 +1,9 @@
+class Graph:
+    
+    # init method or constructor
+    def __init__(self, name):
+        self.name = name
+ 
+    # Sample Method
+    def say_hi(self):
+        print('Hello, my name is', self.name)
